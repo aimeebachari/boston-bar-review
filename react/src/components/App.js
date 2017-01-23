f7f5931c15d0422dc98553a8a6d2e64adbde9e79
Samuel Cole <samcole1992@gmail.com>
2017-01-23 11:56:13 -0500

    Added Up and Down Vote buttons using React

---
+++ react/src/components/App.js
@@ -0,0 +1,15 @@
+/* jshint esversion:6*/
+
+import React, { Component } from 'react';
+import Vote from './Vote';
+// import data from '../constants/data'
+
+const App = (props) => {
+  return(
+    <div>
+      <Vote />
+    </div>
+  );
+};
+
+export default App;
---
+++ react/src/components/Vote.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class Vote extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      upVote: false,
+      downVote: false,
+      sum: 0,
+    }
+    this.handleUpVoteClick = this.handleUpVoteClick.bind(this);
+    this.handleDownVoteClick = this.handleDownVoteClick.bind(this);
+  }
+
+  handleUpVoteClick(){
+    if (!this.state.upVote && !this.state.downVote) {
+      let newSum = this.state.sum + 1;
+      this.setState({
+        upVote: true,
+        sum: newSum
+      })
+    }
+    else if (!this.state.upVote && this.state.downVote) {
+      let newSum = this.state.sum + 2;
+      this.setState({
+        upVote: true,
+        downVote: false,
+        sum: newSum
+      })
+    }
+    else if (this.state.upVote && !this.state.downVote) {
+      let newSum = this.state.sum - 1;
+      this.setState({
+        upVote: false,
+        sum: newSum
+      })
+    }
+  }
+
+  handleDownVoteClick(){
+    if (!this.state.downVote && !this.state.upVote) {
+      let newSum = this.state.sum - 1;
+      this.setState({
+        downVote: true,
+        sum: newSum
+      })
+    }
+    else if (!this.state.downVote && this.state.upVote) {
+      let newSum = this.state.sum - 2;
+      this.setState({
+        downVote: true,
+        upVote: false,
+        sum: newSum
+      })
+    }
+    else if (this.state.downVote && !this.state.upVote) {
+      let newSum = this.state.sum + 1;
+      this.setState({
+        downVote: false,
+        sum: newSum
+      });
+    }
+  }
+  render(){
+    let handleUpVoteClick = () => {this.handleUpVoteClick();};
+    let handleDownVoteClick = () => {this.handleDownVoteClick();};
+    return(
+      <div>
+        <button className = "upVote" onClick = {handleUpVoteClick}>Up</button>
+        <button className = "downVote" onClick = {handleDownVoteClick}>Down</button>
+      </div>
+    )
+  }
+}
+
+export default Vote;
