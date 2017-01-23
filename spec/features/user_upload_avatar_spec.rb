677a44f7ecb89a6ae69dfbad8884739ff24ef1e7
Aimee Bachari <aimeebachari@gmail.com>
2017-01-23 14:42:05 -0500

    finished carrierwave image upload for new and editing users

--- spec/features/user_upload_avatar_spec.rb
+++ spec/features/user_upload_avatar_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "user signs up for account" do
+feature "user uploads image" do
   let (:user) { User.create(
     first_name: "Sonic",
     last_name: "The Hedgehog",
@@ -9,9 +9,36 @@ feature "user signs up for account" do
     password: "password"
     ) }
 
-  visit user_registration_path
-  attach_file('Avatar', File.absolute_path("./public/uploads/user/avatar/5/eamonn.png"))
-  click_on "Update"
+  scenario 'user uploads image and views changes' do
+    login_as_user(user)
+    visit edit_user_registration_path
 
-  expect(page).to have_content("Your account has been updated successfully.")
+    attach_file "Avatar", "#{Rails.root}/public/uploads/user/avatar/5/eamonn.png"
+    fill_in "Current password", with: "password"
+    click_button "Update"
+
+    expect(page).to have_content("Your account has been updated successfully.")
+
+    click_on "Edit profile"
+    expect(page).to have_css("img[src*='eamonn.png']")
+  end
+
+  scenario 'user uploads image at signin' do
+    visit new_user_registration_path
+
+    fill_in "First Name", with: "Sonic"
+    fill_in "Last Name", with: "The Hedgehog"
+    fill_in "Username", with: "Sonic The Hedgehog"
+    fill_in "Email", with: "sonic@hedgehog.com"
+    attach_file "Avatar", "#{Rails.root}/public/uploads/user/avatar/5/eamonn.png"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_on "Create Account"
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+
+    click_on "Edit profile"
+    expect(page).to have_css("img[src*='eamonn.png']")
+  end
 end
