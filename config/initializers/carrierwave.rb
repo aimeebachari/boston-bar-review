339da4fe13deba94f99d08d5248cfe0d3e63d636
Aimee Bachari <aimeebachari@gmail.com>
2017-01-25 13:30:54 -0500

    fixed typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,6 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
     }
-    config.fog_directory  = ENV["boston-bar-review_"]
+    config.fog_directory  = ENV["boston-bar-review-images"]
   end
 end
