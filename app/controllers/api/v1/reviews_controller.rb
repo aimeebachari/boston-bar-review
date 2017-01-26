0e97192a58f034890b03e9f5f1de3d485670b9a9
Samuel Cole <samcole1992@gmail.com>
2017-01-26 15:27:37 -0500

    removed superfluous code

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,30 +1,11 @@
 class Api::V1::ReviewsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  # def index
-  #   @bar = Bar.find(params[:bar_id])
-  #   @reviews = @bar.reviews
-  #   render json: @reviews
-  # end
-  #
   def show
     @bar = Bar.find(params[:bar_id])
     @review = @bar.reviews.find(params[:id])
     render json: @review
   end
-  #
-  # def create
-  #   data = JSON.parse(request.body.read)
-  #   @review = Review.new(rating: data["rating"], body: data["body"])
-  #   if @review.save!
-  #     @bar = Bar.find(params[:bar_id])
-  #     @reviews = @bar.reviews
-  #     @reviews.order(:id)
-  #     render json: @reviews
-  #   else
-  #     render json: {message: "Did not work"}, status: 404
-  #   end
-  # end
 
   def destroy
     @review = Review.find(params[:id])
@@ -32,19 +13,4 @@ class Api::V1::ReviewsController < ApplicationController
     @review.destroy
   end
 
-  # def update
-  #   data = JSON.parse(request.body.read)
-  #   review = Review.find(data["id"])
-  #   if data["type"] == "up_votes"
-  #     review.up_votes += 1
-  #     review.save
-  #     @reviews = Review.order(:id)
-  #     render json: @reviews
-  #   elsif data["type"] == "down_votes"
-  #     review.down_votes += 1
-  #     review.save
-  #     @reviews = Review.order(:id)
-  #     render json: @reviews
-  #   end
-  # end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,6 @@ class ReviewsController < ApplicationController
 
   def index
     @reviews = Review.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @reviews}
-    end
   end
 
 
